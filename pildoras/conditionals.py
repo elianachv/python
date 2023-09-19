ed8f61fe3fb260e5f20c8ffc30d8616c69d7ab50
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-19 09:59:44 -0500

    [Dev][Pildoras-C11][Explanation]: Conditionals part 2
    Add age validation exercise to understand elif usage

--- pildoras/conditionals.py
+++ pildoras/conditionals.py
@@ -1,16 +1,39 @@
 """
 Class 10: Conditionals part 1
+Class 11: Conditionals part 2
 Author: Eliana Chavez
 """
 
 def quiz(score):
-  status="none"
-  if score>7:
-    status="approved"
-  else:
-    status= "failed"
-  return status
+    status = "none"
+    if score > 10 or score < 1:
+        status = "Invalid score"
+        return
+    if score > 7:
+        status = "approved"
+    else:
+        status = "failed"
+    return status
 
 
-print(quiz(3))
-print(quiz(8))
+def validateAge(age):
+    if age >= 65:
+        print("You're an elder")
+    elif (age >= 30 and age < 65):
+        print("You're an adult")
+    elif (age >= 18 and age < 30):
+        print("You're a teen")
+    else:
+        print("You're a kid")
+
+
+def main():
+
+    age = int(input("How old are you? "))
+    validateAge(age)
+
+    score = int(input("What was your exam score? "))
+    print(quiz(score))
+
+
+main()
