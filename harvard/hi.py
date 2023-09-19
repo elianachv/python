ad037df61e5ba8f45023b8640a782e543f0599d9
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-18 22:06:42 -0500

    [Dev][Harvard-C1][Exercise]: Hello world
    Lecture 1: Hello world program and the basis

---
+++ harvard/hi.py
@@ -0,0 +1,15 @@
+"""
+My first python program
+Author: Eliana Chavez
+"""
+
+# Hello world
+print("Hello world!")
+
+# Catching user information
+name = input("What's your name? ")
+print("Hello " + name);
+print("Hello", name, "you're doing \"great\"")
+
+# Python's docs info dor print function
+print("Hello", name, "you're doing \"great\"", sep=" ", end="\n")
