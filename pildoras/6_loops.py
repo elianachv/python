8db2d2f323ff950165efc5fde6b1977c9720817e
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-19 22:46:03 -0500

    [Dev][Pildoras-Cl4][Explanation]: For Loop

---
+++ pildoras/6_loops.py
@@ -0,0 +1,17 @@
+"""
+Class 14: Loops part 1: For
+Author: Eliana Chavez
+"""
+
+# Determined loops : for
+# range(n) could be a list, a tuple, a string, etc
+
+print("Printing 5 times Hello world")
+for i in range(5):
+    print("Hello world!")
+
+print("\n")
+
+print("Printing names in a list")
+for name in ["Isaboth", "Ichimei", "Eli"]:
+    print("Hello", name)
