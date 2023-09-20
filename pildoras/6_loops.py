0a2a53f1ea94939c1ae38c34c559f20e82ed136a
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-19 23:15:50 -0500

    [Dev][Pildoras-Cl5][Explanation]: For Loop part 2

--- pildoras/6_loops.py
+++ pildoras/6_loops.py
@@ -1,5 +1,6 @@
 """
-Class 14: Loops part 1: For
+Class 14: Loops part 1: For basics
+Class 15: Loops part 2: For
 Author: Eliana Chavez
 """
 
@@ -8,10 +9,52 @@ Author: Eliana Chavez
 
 print("Printing 5 times Hello world")
 for i in range(5):
-    print("Hello world!")
+    print("Hello world!", sep="", end=" ")
 
 print("\n")
 
 print("Printing names in a list")
 for name in ["Isaboth", "Ichimei", "Eli"]:
     print("Hello", name)
+
+print("\n")
+
+
+def validateEmail(email):
+
+    emailSyntax = False
+    emailTermination = False
+    count = 0
+
+    for i in email:
+
+        if i == "@" or i == ".":
+            count += 1
+
+    if count >= 2:
+        emailSyntax = True
+
+    for i in (".com", ".es", ".co", ".net"):
+        if str(email).__contains__(i):
+            emailTermination = True
+
+    return emailSyntax and emailTermination
+
+
+def printEmailValidation(email):
+
+    print("Validate email")
+    if validateEmail(email):
+        print(email, "is ok")
+    else:
+        print(email, "is invalid")
+
+
+print("Email validation sample results")
+for email in ("test@gmail.com", "testgmail.com", "test@gamail", "test"):
+    printEmailValidation(email)
+    print("\n")
+
+
+print("Email validation test")
+printEmailValidation(input("What's your email? "))
