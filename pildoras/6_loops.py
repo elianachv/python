f0d0093783218d3d03eafe044de7caa3aae384f8
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-19 23:55:13 -0500

    [Dev][Pildoras-Cl7][Explanation]: While loop

--- pildoras/6_loops.py
+++ pildoras/6_loops.py
@@ -1,32 +1,35 @@
 """
 Class 14: Loops part 1: For basics
 Class 15: Loops part 2: For
-Class 16: range and print tips
+Class 16: Loops part 3: range and print tips
+Class 17: Loops part 4: While
 Author: Eliana Chavez
 """
+import math
 
 # Determined loops : for
 # range(n) could be a list, a tuple, a string, etc
 # Default range(stop)
 
-print("Printing 5 times Hello world")
-for i in range(5):
-    print("Hello world!", sep="", end=" ")
+def printForSamples():
+    print("Printing 5 times Hello world")
+    for i in range(5):
+        print("Hello world!", sep="", end=" ")
 
-print("\n")
+    print("\n")
 
-# range(start,stop,step)
-print("Printing 2 times Hello world using range")
-for i in range(1, 3, 1):
-    print(f"Hello world! {i}")
+    # range(start,stop,step)
+    print("Printing 2 times Hello world using range")
+    for i in range(1, 3, 1):
+        print(f"Hello world! {i}")
 
-print("\n")
+    print("\n")
 
-print("Printing names in a list")
-for name in ["Isaboth", "Ichimei", "Eli"]:
-    print("Hello", name)
+    print("Printing names in a list")
+    for name in ["Isaboth", "Ichimei", "Eli"]:
+        print("Hello", name)
 
-print("\n")
+    print("\n")
 
 
 def validateEmail(email):
@@ -59,11 +62,58 @@ def printEmailValidation(email):
         print(email, "is invalid")
 
 
-print("Email validation sample results")
-for email in ("test@gmail.com", "testgmail.com", "test@gamail", "test"):
-    printEmailValidation(email)
-    print("\n")
+def executeEmailExample():
+
+    print("Email validation sample results")
+    for email in ("test@gmail.com", "testgmail.com", "test@gamail", "test"):
+        printEmailValidation(email)
+        print("\n")
+
+    print("Email validation test")
+    printEmailValidation(input("What's your email? "))
+
+# Undetermined loops : while
 
 
-print("Email validation test")
-printEmailValidation(input("What's your email? "))
+def printWhileSamples():
+    i = 1
+    while i <= 10:
+        print("Execution", i)
+        i += 1
+
+    age = int(input("How old are you? "))
+    while 0 < age < 100:
+        print("Valid age", age)
+        age = int(input("How old are you? "))
+
+    print("Thanks")
+
+
+def calculateSqureRoot():
+    attempt = 0
+    number = int(input("Digit number: "))
+
+    while number < 0:
+        print("Error, square root cannot be calculated for negative values")
+
+        if attempt == 2:
+            print("You've have tried so many times")
+            break
+
+        number = int(input("Digit number: "))
+        if number < 0:
+            attempt += 1
+
+    if attempt < 2:
+        solution = math.sqrt(number)
+        print(f"square root of {number} is {solution}")
+
+
+def main():
+    # printForSamples()
+    # executeEmailExample()
+    # printWhileSamples()
+    calculateSqureRoot()
+
+
+main()
