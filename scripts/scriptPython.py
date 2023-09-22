157ab8e8f6ab7c9f70a09facdda8795f866744e7
Eliana Chavez <elianachavezv@gmail.com>
2023-09-22 01:19:42 +0000

    [DevOps][Bootcamp-C8][Exercises]: Scripts
    First scripts using python

---
+++ scripts/scriptPython.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+print("Hello world!")
+
+print(
+""" 
+    This is a template 
+    and I can take all your
+    line jumps
+    I'm extreamly useful for making docs
+    ________________________________________________
+    XXXXXXXXXXXXX BEATIFUL DOCS XXXXXXXXXXXXXXXXXXXX
+    ________________________________________________
+    Example: --version is a common flag to get the
+    actual version of any command or app
+    ________________________________________________
+""")
+
+
+print("This","is","a","segmented","string")
+
+
