0364f34eeaef6e2a6d729f16e4d2ab0363741438
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-22 00:48:31 -0500

    [Dev][Pildoras-C29][Explanation]: Inheritance basics
    New example superclass Vehicle subclass Motorycle
    Inheritance syntax and basic application using parent class methods

--- pildoras/9_poo.py
+++ pildoras/9_poo.py
@@ -1,8 +1,10 @@
 """
 Class 26: Class concepts
 Class 27 - 28: Constructors, encapsulation
+Class 29 - 31: Inheritance 
 Author: Eliana Chavez
 """
+# POO basics example
 
 
 class Car():
@@ -71,14 +73,14 @@ class Car():
             return False
 
 
-def main():
+def pooExample():
     # Create instance of a class
     car = Car()
     car2 = Car()
 
     # Accessing attributes
     print("Try to modify number of wheels car 1")
-    car.__wheels = 2 # Attribute won't be changed
+    car.__wheels = 2  # Attribute won't be changed
 
     print(f"Car1: {car.info()}", f"Car2: {car2.info()}", sep="\n")
 
@@ -92,6 +94,81 @@ def main():
     # car2.__check() # Generates error
     print(f"Car 1 status: {car.getStatus()}")
     print(f"Car 2 status: {car2.getStatus()}")
+
+# Inheritance example
+
+
+class Vehicle():
+
+    def __init__(self, trademark, model, wheels):
+        self.trademark = trademark
+        self.model = model
+        self.__wheels = wheels
+        self.__agoing = False
+
+    def start(self):
+        if self.__agoing:
+            print("Vehicle is already started")
+        else:
+            if self.__check():
+                print("starting")
+                self.__agoing = True
+                print("started")
+            else:
+                print("Vehicle cannot be started. Check it")
+
+        return self.getStatus()
+
+    def stop(self):
+        if self.__agoing:
+            print("stopping")
+            self.__agoing = False
+            print("stopped")
+        else:
+            print("Vehicle is already stopped")
+
+        return self.getStatus()
+
+    def getStatus(self):
+        if self.__agoing:
+            return "Vehicle is started"
+        else:
+            return "Vehicle is stopped"
+
+    def info(self):
+        return f"""
+              This vehicle:
+              Trademark: {self.trademark}
+              Model: {self.model}
+              Number of wheels: {self.__wheels}
+              Status: {self.getStatus()}
+              """
+
+    def __check(self):
+        print("Checking car status")
+        self.fuel = "ok"
+        self.oil = "low"
+        self.doors = "closed"
+        if self.fuel == "ok" and self.oil == "ok" and self.doors == "closed":
+            return True
+        else:
+            return False
+
+# Class inherit from other
+
+
+class Motorcycle(Vehicle):
+    pass
+
+
+def inheritanceExample():
+    motorcycle1 = Motorcycle("Honda", "CBR", 2)
+    print(motorcycle1.info())
+
+
+def main():
+    # pooExample()
+    inheritanceExample()
 
 
 main()
