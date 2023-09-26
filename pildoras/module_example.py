51083d24bab67bf1f90643226a33ba62c65956ea
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-25 20:59:03 -0500

    [Dev][Pildoras-C34][Explanation]: Modules example

---
+++ pildoras/module_example.py
@@ -0,0 +1,51 @@
+"""
+Class 34: Modules
+Module file
+Author: Eliana Chavez
+"""
+# Modules extensions .py .pyc
+# It helps to organize and reuse code
+
+def add(num1, num2):
+
+    if isinstance(num1, int) == False:
+        num1 = int(num1)
+
+    if isinstance(num2, int) == False:
+        num2 = int(num2)
+
+    return num1 + num2
+
+def substract(num1, num2):
+
+    if isinstance(num1, int) == False:
+        num1 = int(num1)
+
+    if isinstance(num2, int) == False:
+        num2 = int(num2)
+
+    return num1 - num2
+
+def multiply(num1, num2):
+
+    if isinstance(num1, int) == False:
+        num1 = int(num1)
+
+    if isinstance(num2, int) == False:
+        num2 = int(num2)
+
+    return num1 * num2
+
+def divide(num1, num2):
+
+    if isinstance(num1, int) == False:
+        num1 = int(num1)
+
+    if isinstance(num2, int) == False:
+        num2 = int(num2)
+
+    if num2 == 0:
+        return 0;
+
+    return num1 / num2
+
