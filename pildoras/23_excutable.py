b37eb9fd1945f0aa82189c0b4d4801c20408e95e
Eliana Chavez Valencia <elianachavezv@gmail.com>
2023-09-30 13:40:52 -0500

    [Dev][Pildoras-C78][Explanation]: Executables

---
+++ pildoras/23_excutable.py
@@ -0,0 +1,16 @@
+"""
+Class 78: Executables
+Author: Eliana Chavez
+"""
+
+# 1. execute pip install pyinstaller
+# 2. Access to directory of the python app
+# 3. execute pyinstaller --windowed --onefile --icon=logo_path app_name
+# windowed means console won't appear
+# onefile means whole app will be inside .exe file
+# icon: If you want the .exe file to have an icon you must have a .ico img inside the app folder
+# 4. Access dist folder
+# 5. You will find your .exe inside
+
+
+
